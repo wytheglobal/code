e87c535fbb84fa86230a1c95d1e35d651fad9e1e
yu <wangyu0248@gmail.com>
2022-04-18 02:00:25 +0800

    feat: TODO NetPrice() 这里应该还是有问题，直接返回的是基类

---
+++ design_pattern/foundation_class.cpp
@@ -0,0 +1,140 @@
+const long DEFAULT_LIST_CAPACITY = 200;
+
+/*
+*/
+//typedef int bool;
+//const int true = 1;
+//const int false = 0;
+/*
+template <class Item>
+class List {
+public:
+    List(long size = DEFAULT_LIST_CAPACITY);
+    List(List&);
+    ~List();
+    List& operator=(const List&);
+
+    long Count() const;
+    Item& Get(long index) const;
+    Item& First() const;
+    Item& Last() const;
+    bool Includes(const Item&) const;
+
+    void Append(const Item&);
+    void Prepend(const Item&);
+
+    void Remove(const Item&);
+    void RemoveLast();
+    void RemoveFirst();
+    void RemoveAll();
+
+    Item& Top() const;
+    void Push(const Item&);
+    Item& Pop();
+};
+*/
+template <class Item>
+class Node {
+public:
+    Node(const Item& item) : data(item) {};
+    Node *next;
+    const Item& data;
+};
+
+// TODO
+// why struct doesn't work in my case
+// https://gist.github.com/unreadable/fcf83725fb80a6790a43426940644523
+template <class Item>
+class List {
+public:
+    List();
+    const Item& Get(long index) const;
+    void Append(const Item&);
+
+    long Count() const;
+private:
+    Node<Item> *head;
+    Node<Item> *tail;
+    long _count;
+};
+
+template <class Item>
+List<Item>::List() {
+    head = NULL;
+    tail = NULL;
+    _count = 0;
+};
+
+template <class Item>
+const Item& List<Item>::Get(long index) const {
+    Node<Item> *ptr = head;
+    while (index > 0) {
+        index--;
+        ptr = ptr->next;
+    }
+    const Item& item = ptr->data;
+    return item;
+};
+
+template <class Item>
+void List<Item>::Append(const Item& item) {
+    Node<Item> *n = new Node<Item>(item);
+    std::cout << _count << std::endl;
+    if (_count == 0) {
+        n->next = NULL;
+        head = n;
+        tail = n;
+    } else {
+        tail->next = n;
+        tail = n;
+    }
+    _count++;
+};
+
+template <class Item>
+long List<Item>::Count() const {
+    return _count;
+};
+
+
+/*
+template <typename T>
+struct Node {
+  T& data;
+  Node *next;
+};
+
+template <typename T> class List{
+private:
+    Node<T> *head;
+public:
+    List(){
+        head = NULL;
+    }
+
+    void push(T& val){
+        Node<T> *n = new Node<T>();
+        n->data = val;
+        n->next = head;
+        head = n;
+    }
+
+    T pop(){
+      if(head) {
+        T p = head->data;
+        head = head->next;
+        return p;
+      }
+    }
+
+    bool search(T val) {
+      Node<T> *temp = head;
+      while(temp->next) {
+        if(temp->data == val) return true;
+        else temp = temp->next;
+      }
+      delete temp;
+      return false;
+    }
+};
+*/
--- design_pattern/structure/composite/composite.cpp
+++ design_pattern/structure/composite/composite.cpp
@@ -1,23 +1,72 @@
 #include <iostream>
-#include <iterator>
+
+#include "../../foundation_class.cpp"
+
 
 using namespace std;
 
 typedef int Watt;
 typedef int Currency;
 
+// class IteratorOutOfBounds : public exception {};
+/**
+ * This is abstract class
+ * There is no need to implement abstract class
+ * It's implemented by it's sub class
+ */
 template <class Item>
 class Iterator {
 public:
     virtual void First() = 0;
     virtual void Next() = 0;
     virtual bool IsDone() const = 0;
-    virtual Item Currentltem() const = 0;
+    virtual Item CurrentItem() const = 0;
 };
+
+template <class Item>
+class ListIterator : public Iterator<Item> {
+public:
+    ListIterator(const List<Item>* aList);
+    virtual void First();
+    virtual void Next();
+    virtual bool IsDone() const;
+    virtual Item CurrentItem() const;
+private:
+    const List<Item>* _list;
+    long _current;
+};
+
+template <class Item>
+ListIterator<Item>::ListIterator (const List<Item>* aList) : _list(aList), _current(0) {};
+
+template <class Item>
+void ListIterator<Item>::First() {
+    _current = 0;
+};
+
+template <class Item>
+void ListIterator<Item>::Next() {
+    _current++;
+};
+
+template <class Item>
+bool ListIterator<Item>::IsDone () const {
+    return _current >= _list->Count();
+};
+
+template <class Item>
+Item ListIterator<Item>::CurrentItem () const {
+    if (IsDone()) {
+        // throw IteratorOutOfBounds;
+        throw std::range_error("AddPositiveIntege");
+    }
+    std::cout << "itrator" << _current << "total:" << _list->Count() << std::endl;
+    return _list->Get(_current);
+}
 
 class Equipment {
 public:
-    const char* Name()
+    const char* Name() const
         { return _name; };
     virtual Watt Power()
         {  return 10; };
@@ -30,15 +79,118 @@ public:
         { cout << "add equipment" << endl ;  };
     virtual void Remove(Equipment*)
         { cout << "Remove equipment" << endl ;  };
-    virtual Iterator<Equipment*>* Createlterator()
-        { return Iterator; };
-// protected:
+    virtual Iterator<Equipment>* CreateIterator() {};
+protected:
     Equipment(const char* n) : _name(n) {};
 private:
     const char* _name;
 };
 
+class CompositeEquipment: public Equipment {
+public:
+    // virtual ~CompositeEquipment();
+
+    // virtual Watt Power();
+    virtual Currency NetPrice();
+    // virtual Currency DiscountPrice();
+
+    virtual void Add(Equipment*);
+    virtual void Remove(Equipment*);
+    virtual Iterator<Equipment>* CreateIterator();
+
+protected:
+    CompositeEquipment(const char* name) : Equipment(name) {
+        _equipment = new List<Equipment>;
+    };
+private:
+    List<Equipment>* _equipment = nullptr;
+};
+
+Iterator<Equipment>* CompositeEquipment::CreateIterator() {
+    return new ListIterator<Equipment>(_equipment);
+};
+
+void CompositeEquipment::Add(Equipment * item){
+    // _equipment->Append(item);
+    // std::cout <<  Name() << " add -> " << item->Name() << std::endl;
+    _equipment->Append(*item);
+}
+void CompositeEquipment::Remove(Equipment* item) {
+
+}
+
+
+Currency CompositeEquipment::NetPrice() {
+    Iterator<Equipment>* i = CreateIterator();
+    Currency total = 0;
+
+    for (i->First(); !i->IsDone(); i->Next()) {
+        Equipment item = i->CurrentItem();
+        std::cout << "name: " << item.Name() << " price:" << item.NetPrice() << std::endl;
+        total += item.NetPrice();
+    }
+    delete i;
+    return total;
+}
+
+/*
+ * Chassis 底盘、底架
+ */
+class Chassis : public CompositeEquipment {
+public:
+    Chassis(const char* name) : CompositeEquipment(name) {};
+    // virtual ~Chassis();
+/*
+*/
+    // virtual Watt Power();
+    // virtual Currency NetPrice();
+    // virtual Currency DiscountPrice();
+};
+
+// Cabinet 机箱
+class Cabinet  : public CompositeEquipment {
+public:
+    Cabinet(const char* name) : CompositeEquipment(name) {};
+};
+class Bus  : public CompositeEquipment {
+public:
+    Bus(const char* name) : CompositeEquipment(name) {};
+};
+class Card  : public Equipment {
+public:
+    Card(const char* name) : Equipment(name) {};
+};
+
+
+class FloppyDisk : public Equipment {
+public:
+    FloppyDisk(const char* name) : Equipment(name) {};
+    // virtual ~FloppyDisk();
+/*
+*/
+    // virtual Watt Power();
+    // virtual Currency NetPrice();
+    // virtual Currency DiscountPrice();
+};
 
 int main() {
-    Equipment* e = new Equipment("hello");
+    // CompositeEquipment* e = new CompositeEquipment("mother_board");
+    // cabinet 机箱
+    Cabinet* cabinet = new Cabinet("PC Cabinet");
+    // chasis 底盘
+    Chassis* chassis = new Chassis("PC Chassis");
+    /*
+    */
+    cabinet->Add(chassis);
+
+    Bus* bus = new Bus("MCA Bus");
+    bus->Add(new Card("16Mbs Token Ring"));
+
+    FloppyDisk* floppydisk = new FloppyDisk("3.5in Floppy");
+
+    chassis->Add(bus);
+    chassis->Add(floppydisk);
+
+    cout << "The net price is " << std::endl;
+    cout << chassis->NetPrice() << endl;
 }
